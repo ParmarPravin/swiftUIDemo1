328e36cc7d51307c623ab3078dc0e590f024608d
Pravin_OPL <pravin.parmar@onlinepsbloans.com>
2024-11-11 18:26:36 +0530

    add setting file

---
+++ SwiftUIDemo1/Login/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  SwiftUIDemo1
+//
+//  Created by Pravin Parmar on 11/11/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
