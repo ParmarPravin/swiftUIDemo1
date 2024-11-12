4fd514361a2fdda0d8d04d5f50d18ee2b0f94601
Pravin_OPL <pravin.parmar@onlinepsbloans.com>
2024-11-12 18:13:34 +0530

    add test111

---
+++ SwiftUIDemo1/Login/test111.swift
@@ -0,0 +1,18 @@
+//
+//  test111.swift
+//  SwiftUIDemo1
+//
+//  Created by Pravin Parmar on 12/11/24.
+//
+
+import SwiftUI
+
+struct test111: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test111()
+}
